dd6a34fe50653c6f0e8c7a82ba5104c14e5d7519
Elijah Davis <elijahgdavis1999@gmail.com>
2021-01-03 18:12:43 -0800

    Added local storage

--- script.js
+++ script.js
@@ -9,12 +9,20 @@ function stateInfo(state) {
     // Creating a div to hold the movie
     var results = $("#covidResults");
     results.html(`  <div class="card-content white-text">
-    <span class="card-title">COVID-19 INFO</span>
+    <span class="card-title">COVID-19 INFO for ${localStorage
+      .getItem("lastState")
+      .toUpperCase()}</span>
     <p>
-      <h5 class="hospCurrent">Hospitalized Currently: ${response.hospitalizedCurrently}</h3><hr>
-      <h5 class="hospIncrease">Hospitalized Increase: ${response.hospitalizedIncrease}</h3><hr>
+      <h5 class="hospCurrent">Hospitalized Currently: ${
+        response.hospitalizedCurrently
+      }</h3><hr>
+      <h5 class="hospIncrease">Hospitalized Increase: ${
+        response.hospitalizedIncrease
+      }</h3><hr>
       <h5 class="deathTotal">Deaths: ${response.death}</h3><hr>
-      <h5 class="deathIncrease">Deaths Increase: ${response.deathIncrease}</h3><hr>
+      <h5 class="deathIncrease">Deaths Increase: ${
+        response.deathIncrease
+      }</h3><hr>
     </p>
   </div>
   <div class="infoLink card-action">
@@ -33,9 +41,6 @@ function displayStateInfo(event) {
   stateInfo(state);
 }
 
-$(".stateBtn").on("click", displayStateInfo);
-stateInfo();
-
 function buildQueryURL() {
   // queryURL is the url we'll use to query the API
   var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
@@ -45,9 +50,7 @@ function buildQueryURL() {
   var queryParams = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M" };
 
   // Grab text the user typed into the search input, add to the queryParams object
-  queryParams.q = $("#search-term")
-    .val()
-    .trim();
+  queryParams.q = $("#search-term").val().trim();
 
   // Logging the URL so we have access to it for troubleshooting
   console.log("---------------\nURL: " + queryURL + "\n---------------");
@@ -60,7 +63,7 @@ function buildQueryURL() {
  */
 function updatePage(NYTData) {
   // Get from the form the number of results to display, set limit
-  
+
   var numArticles = $("#article-count").val();
 
   // Loop through and build elements for the defined number of articles
@@ -102,28 +105,39 @@ function updatePage(NYTData) {
 
     var leadParagraph = article.lead_paragraph;
     if (leadParagraph && article.lead_paragraph) {
-      $articleListItem.append("<h6><strong>Lead Paragraph:</strong> " + article.lead_paragraph + "</h6>");
-
+      $articleListItem.append(
+        "<h6><strong>Lead Paragraph:</strong> " +
+          article.lead_paragraph +
+          "</h6>"
+      );
     }
     var snippet = article.snippet;
     if (snippet) {
-      $articleListItem.append("<h6><strong>Snippet:</strong> " + article.snippet + "</h6>");
+      $articleListItem.append(
+        "<h6><strong>Snippet:</strong> " + article.snippet + "</h6>"
+      );
     }
 
     // Log section, and append to document if exists
     var section = article.section_name;
     if (section) {
-      $articleListItem.append("<h6><strong>Section:</strong> " + section + "</h6>");
+      $articleListItem.append(
+        "<h6><strong>Section:</strong> " + section + "</h6>"
+      );
     }
 
     // Log published date, and append to document if exists
     var pubDate = article.pub_date;
     if (pubDate) {
-      $articleListItem.append("<h6><strong>Publish Date:</strong> " + article.pub_date + "</h6>");
+      $articleListItem.append(
+        "<h6><strong>Publish Date:</strong> " + article.pub_date + "</h6>"
+      );
     }
 
     // Append and log url
-    $articleListItem.append("<a href='" + article.web_url + "'>" + article.web_url + "</a>");
+    $articleListItem.append(
+      "<a href='" + article.web_url + "'>" + article.web_url + "</a>"
+    );
 
     // Append the article
     $articleList.append($articleListItem);
@@ -135,11 +149,15 @@ function clear() {
   $("#article-section").empty();
 }
 
+if (localStorage.getItem("lastState") === null) {
+  localStorage.setItem("lastState", "ca");
+}
+
 // CLICK HANDLERS
 // ==========================================================
 
 // .on("click") function associated with the Search Button
-$("#run-search").on("click", function(event) {
+$("#run-search").on("click", function (event) {
   // (in addition to clicks). Prevents the page from reloading on form submit.
   event.preventDefault();
 
@@ -153,9 +171,12 @@ $("#run-search").on("click", function(event) {
   // The data then gets passed as an argument to the updatePage function
   $.ajax({
     url: queryURL,
-    method: "GET"
+    method: "GET",
   }).then(updatePage);
 });
 
 //  .on("click") function associated with the clear button
 $("#clear-all").on("click", clear);
+
+$(".stateBtn").on("click", displayStateInfo);
+stateInfo(localStorage.getItem("lastState"));
