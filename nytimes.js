56fb2a055af6159197b1b7ff2abaf239fa366221
Guled Ahmed <akindnesstoremember@gmail.com>
2021-01-04 11:46:27 -0800

    re-added the API code containing the comments into the nytimes.js file

--- nytimes.js
+++ nytimes.js
@@ -1,29 +1,39 @@
 function buildQueryURL() {
-
+    // Using queryURL to query API
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
   
+    // Building an object to contain API call's query parameters and setting API key
     var queryParams = { "api-key": "R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M" };
   
+    // Grab text the user typed into the search input, add to the queryParams object
     queryParams.q = $("#search-term").val().trim();
   
+    // Returning the query
     return queryURL + $.param(queryParams);
   }
   
+  // Creating function for API data (JSON/Object), turning it into elements on page
   function updatePage(NYTData) {
   
+    // Get from the form the number of results to display, set limit
     var numArticles = $("#article-count").val();
   
+    // Loop through and build elements for the defined number of articles
     for (var i = 0; i < numArticles; i++) {
-
-        var article = NYTData.response.docs[i];
+      // Get specific article info for current index
+      var article = NYTData.response.docs[i];
   
+      // Increase the articleCount (track article # - starting at 1)
       var articleCount = i + 1;
   
+      // Create the  list group to contain the articles and add the article content for each
       var $articleList = $("<ul>");
       $articleList.addClass("list-group");
   
+      // Add the newly created element to the DOM
       $("#article-section").append($articleList);
   
+      // If the article has a headline, log and append to $articleList
       var headline = article.headline;
       var $articleListItem = $("<li class='list-group-item articleHeadline'>");
   
@@ -38,6 +48,7 @@ function buildQueryURL() {
         );
       }
   
+      // If the article has a byline, log and append to $articleList
       var byline = article.byline;
   
       if (byline && byline.original) {
@@ -53,6 +64,7 @@ function buildQueryURL() {
         );
       }
   
+      // Log snippet and append to document
       var snippet = article.snippet;
       if (snippet) {
         $articleListItem.append(
@@ -60,6 +72,7 @@ function buildQueryURL() {
         );
       }
   
+      // Log section, and append to document if exists
       var section = article.section_name;
       if (section) {
         $articleListItem.append(
@@ -67,6 +80,7 @@ function buildQueryURL() {
         );
       }
   
+      // Log published date, ""
       var pubDate = article.pub_date;
       if (pubDate) {
         $articleListItem.append(
@@ -74,24 +88,30 @@ function buildQueryURL() {
         );
       }
   
+      // Append and log url
       $articleListItem.append(
         "<a href='" + article.web_url + "'>" + article.web_url + "</a>"
       );
   
+      // Append the article
       $articleList.append($articleListItem);
     }
   }
 
+// .on("click") function associated with the Search Button
 $("#run-search").on("click", function (event) {
+    // (in addition to clicks). Prevents the page from reloading on form submit.
     event.preventDefault();
-  
+
+    // Empty the region associated with the articles
     clear();
-  
+
+    // Build the query URL for the ajax request to the NYT API
     var queryURL = buildQueryURL();
-  
+
+    // Creating the AJAX request to API getting JSON data at queryURL, create Ajax for passing data to updatePage function
     $.ajax({
-      url: queryURL,
-      method: "GET",
+    url: queryURL,
+    method: "GET",
     }).then(updatePage);
-  });
-  
+});
